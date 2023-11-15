52108ba7ef971d99c466631d4badfe4961381e49
bobcamps065 <brunocamps1@hotmail.com>
2023-11-15 11:34:44 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('btn').addEventListener('click', function myFunc() {
+    this.style.background = "green"
+    this.style.color = "yellow"
+    this.style.width = "99px"
+    this.innerText = "OK"
+    
+    
+})
+
+
+let promise = fetch('',)
